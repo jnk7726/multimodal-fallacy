0dcfdd0f7edeafa154431544efaa8d791e988c52
jnk7726 <jaynitin.kaoshik2020@vitalum.ac.in>
2025-05-16 20:27:57 -0400

    Update data_loading.py

--- Code_AFC/mamkit/training_scripts/data_loading.py
+++ Code_AFC/mamkit/training_scripts/data_loading.py
@@ -12,14 +12,6 @@ def loading_data_example():
                            base_data_path=base_data_path)
     logging.info(loader.data)
 
-
-def loading_predefined_splits():
-    loader = UKDebates(task_name='asd',
-                       input_mode=InputMode.TEXT_ONLY)
-    split_info = loader.get_splits('mancini-et-al-2022')
-    logging.info(split_info[0])
-
-
 def custom_splits(
         self,
 ) -> List[SplitInfo]:
@@ -28,16 +20,6 @@ def custom_splits(
     test_df = self.data.iloc[100:]
     fold_info = self.build_info_from_splits(train_df=train_df, val_df=val_df, test_df=test_df)
     return [fold_info]
-
-
-def defining_custom_splits_example():
-    loader = UKDebates(task_name='asd',
-                       input_mode=InputMode.TEXT_ONLY)
-    loader.add_splits(method=custom_splits,
-                      key='custom')
-
-    split_info = loader.get_splits('custom')
-    logging.info(split_info[0])
 
 
 if __name__ == '__main__':
